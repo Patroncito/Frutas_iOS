7e25b89f5ae33e5d4928e6a971bb38d32f2d86c5
Alfonso Patron <c9alfonsopatron@hotmail.com>
2024-01-08 16:26:40 -0700

    Add iPad Compatibility

---
+++ Frutas/App/SettingsRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsRowView.swift
+//  Frutas
+//
+//  Created by Alfonso Patron on 08/01/24.
+//
+
+import SwiftUI
+
+struct SettingsRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsRowView()
+}
